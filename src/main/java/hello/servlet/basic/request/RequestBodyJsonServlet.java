36ffdb4ea89aaf389cb4ac2e92947a605689818a
hyejin <obndo8539@naver.com>
2023-12-08 10:49:56 +0900

    Http 응답 방식

--- src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -22,7 +22,7 @@ import java.nio.charset.StandardCharsets;
  * message body: {"username" : "hello", "age" : 20}
  *
  */
-@WebServlet(name="RequestBodyJsonServlet", urlPatterns = "/request-body-json")
+@WebServlet(name="requestBodyJsonServlet", urlPatterns = "/request-body-json")
 public class RequestBodyJsonServlet extends HttpServlet {
 
     private ObjectMapper objectMapper = new ObjectMapper();
